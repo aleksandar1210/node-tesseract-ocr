8ff67ba98fa81c76376f73de0ea44aed512acaca
fanghuateng0621 <fanghuateng0621@gmail.com>
2024-11-11 16:41:58 -0800

    ocr script in ch

---
+++ index.js
@@ -0,0 +1,18 @@
+const tesseract = require("node-tesseract-ocr");
+
+const config = {
+  lang: "eng",
+  oem: 1,
+  psm: 3,
+};
+
+const img = "./test.png";
+
+tesseract
+  .recognize(img, config)
+  .then((text) => {
+    console.log("Result:", text);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
